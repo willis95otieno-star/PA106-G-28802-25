f40cd1e95ca4037110d45a56e5b0c9c3d3df6bd4
Willis otieno <willis95otieno@gmail.com>
2025-10-30 10:58:05 +0300

    Update branch_managment.c
    sorry mwalimu I forgot the sum part I've just rectified it

--- branch_managment.c
+++ branch_managment.c
@@ -9,7 +9,7 @@ vacant in the three branches*/
  
 
 int main() {
-    int chain[3][5][10],occupancy[5][10];
+    int chain[3][5][10],occupancy[5][10],sum=0;
     srand(time(0));
     for(int b=0; b<3;b++) {
         printf("branch %d\n",b+1);
@@ -18,12 +18,14 @@ int main() {
             for (int r = 0; r < 10; r++) {
               printf("            room%-3d:",r+1);
             if (occupancy[f][r]=rand()%2){
+             sum=sum+1;
              printf("occupied\n");}
             else{
              printf("vacant\n");}
         }
         }
         }
+        printf("\ntotal number of room occupied %d \n",sum);
 
     return 0;
 }
